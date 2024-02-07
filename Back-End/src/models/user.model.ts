910794addf2c353fa548167ba6949b6dada290bc
DhanushkaTa <dhanushka999ta@gmail.com>
2024-02-07 17:32:43 +0530

    create user model

--- Back-End/src/models/user.model.ts
+++ Back-End/src/models/user.model.ts
@@ -1,3 +1,42 @@
 import {DataTypes, Sequelize} from "sequelize";
 import sequelize from "../db";
 
+const UserModel = sequelize.define("user",{
+    nic:{
+        type: DataTypes.STRING(10),
+        primaryKey: true,
+        allowNull: false,
+        validate:{
+            notEmpty:true
+        }
+    },
+    username:{
+        type: DataTypes.STRING,
+        unique: true,
+        allowNull: false
+    },
+    fullName:{
+        type: DataTypes.STRING(50),
+        allowNull: false,
+        validate:{
+            notEmpty:true
+        }
+    },
+    email:{
+        type: DataTypes.STRING,
+        unique: true,
+        allowNull: false,
+        validate: {
+            isEmail: true
+        }
+    },
+    age: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        validate:{
+            isNumeric: true
+        }
+    }
+})
+
+export default UserModel;
